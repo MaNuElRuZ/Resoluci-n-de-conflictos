a6bf1ea71ee9ad9288fb330cbb66325d863e4046
Manuel Ruz <manuel22ene@gmail.com>
2020-11-12 18:39:38 +0100

    primer paso casa

--- Resolución de conflictos/src/Resolución.java
+++ Resolución de conflictos/src/Resolución.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class Resolución {
     public static void main(String[] args) {
         System.out.println("hola");
+        System.out.println("que tal");
     }
 }
